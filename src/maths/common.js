783167be9254a5df24930a700e45fef8d7168792
patiboh <lapatiboh@gmail.com>
2021-02-20 09:29:46 +0100

    🧽  Use Math.sign

--- src/maths/common.js
+++ src/maths/common.js
@@ -15,14 +15,6 @@ const common = {
   isScalar(n) {
     return typeof n === 'number' && !Number.isNaN(n)
   },
-
-  /**
-   * @param {number|Object} n
-   * @returns {boolean}
-   */
-  isNeg(n) {
-    return n !==0 && n + Math.abs(n) === 0;
-  },
   
   /**
    * @param {number|string|Object} a
